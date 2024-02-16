5d5ca832e996e4af2c2642743f905c3453ca3848
tituz175 <tobititus01@gmail.com>
2024-02-15 19:52:51 -0700

    task modification done

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,14 +1,17 @@
 #!/usr/bin/node
-const axios = require('axios').default;
+const request = require('request');
 const movieId = process.argv[2];
 
-async function fetch (url) {
-  try {
-    const response = await axios.get(url);
-    return response;
-  } catch (error) {
-    console.error(error);
-  }
+function fetch (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, res, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body));
+      }
+    });
+  });
 }
 
 const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
@@ -17,9 +20,9 @@ async function getMovieCharacters () {
   try {
     const characters = [];
     const movieData = await fetch(movieUrl);
-    for (const url of movieData.data.characters) {
+    for (const url of movieData.characters) {
       const characterData = await fetch(url);
-      characters.push(characterData.data.name);
+      characters.push(characterData.name);
     }
     characters.forEach(element => {
       console.log(element);
