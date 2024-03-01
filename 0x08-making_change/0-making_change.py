dcb707326e5b146256de6a0079d5dc0b39552501
tituz175 <tobititus01@gmail.com>
2024-02-29 21:49:59 -0700

    task done

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
 def makeChange(coins, total):
-    if total <= 0:
-        return 0
-    coins = sorted(coins)
-    count = 0
-    num = total - coins.pop()
-    count += 1
-    for item in reversed(coins):
-        if num % item == 0:
-            return (count + (num // item))
-    return -1
+    max_val = float('inf')
+    table = [[max_val for _ in range(total + 1)] for _ in range(len(coins))]
+
+    for i in range(len(coins)):
+        for j in range(total + 1):
+            if j == 0:
+                table[i][j] = 0
+            elif coins[i] <= j:
+                if i == 0:
+                    table[i][j] = table[i][j - coins[i]] + \
+                        1 if j % coins[i] == 0 else max_val
+                else:
+                    table[i][j] = min(
+                        table[i - 1][j], table[i][j - coins[i]] + 1)
+            else:
+                if i > 0:
+                    table[i][j] = table[i - 1][j]
+    return table[-1][-1] if table[-1][-1] != float('inf') else -1
